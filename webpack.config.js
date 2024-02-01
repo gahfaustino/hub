f818215d3d056f71393fa9f3af9aa5ec32ccbf10
gahfaustino <tech.03@bpone.group>
2024-01-31 22:44:07 -0300

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const Dotenv = require('dotenv-webpack');
+const deps = require("./package.json").dependencies;
+module.exports = (_, argv) => ({
+  output: {
+    publicPath: "http://localhost:3000/",
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".jsx", ".js", ".json"],
+  },
+
+  devServer: {
+    port: 3000,
+    historyApiFallback: true,
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.m?js/,
+        type: "javascript/auto",
+        resolve: {
+          fullySpecified: false,
+        },
+      },
+      {
+        test: /\.(css|s[ac]ss)$/i,
+        use: ["style-loader", "css-loader", "postcss-loader"],
+      },
+      {
+        test: /\.(ts|tsx|js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        },
+      },
+    ],
+  },
+
+  plugins: [
+      new ModuleFederationPlugin({
+          name: "container",
+          filename: "containerEntry.js",
+          remotes: {
+              "AppContainer": "LunareHubHome@https://host-of-component/remoteEntry.js"
+          },
+          shared: {
+             ...deps
+          }
+      }),
+    new HtmlWebPackPlugin({
+      template: "./public/index.html",
+    }),
+    new Dotenv()
+  ],
+});
